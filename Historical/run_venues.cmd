c6ce0d55991832e38395bbdda9d3d3b60a2d937d
LAPTOP-D62CUIFL\user <khalifea.sy@gmail.com>
2026-09-24 02:46:25 +0800

    modify run_venues.cmd

--- Historical/run_venues.cmd
+++ Historical/run_venues.cmd
@@ -1,24 +1,24 @@
 @echo off
 cd /d "%~dp0"
 
-python historical_ticks.py --venues "ASX-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "BSE-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "HKG-GEM" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "HKG-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "JKT-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "KLS-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "KOE-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "KSC-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "NSI-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "NZX-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "PHS-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "SES-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "SET-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "SHA-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "SHH-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "SHZ-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "SSC-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "SZA-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "SZC-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "TAI-MAIN" --backfill 31 --chunk 30 --compress_venues
-python historical_ticks.py --venues "TYO-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "ASX-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "BSE-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "HKG-GEM" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "HKG-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "JKT-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "KLS-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "KOE-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "KSC-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "NSI-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "NZX-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "PHS-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "SES-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "SET-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "SHA-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "SHH-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "SHZ-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "SSC-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "SZA-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "SZC-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "TAI-MAIN" --backfill 31 --chunk 30 --compress_venues
+python .\Historical\historical_ticks.py --venues "TYO-MAIN" --backfill 31 --chunk 30 --compress_venues
